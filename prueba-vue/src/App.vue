582bf0efd00a61de0c397aedb45ba9144e3454d3
Nessa <vanessacardenasmelendez@hotmail.com>
2022-11-03 19:25:55 -0500

    Agregar VUE

---
+++ prueba-vue/src/App.vue
@@ -0,0 +1,32 @@
+<template>
+  <nav>
+    <h1 class="HomeroTitulo">Sistema de Rastreo</h1>
+  </nav>
+  <router-view/>
+</template>
+
+<style>
+.HomeroTitulo{
+  color :rgb(181, 47, 218)
+}
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+}
+
+nav {
+  padding: 30px;
+}
+
+nav a {
+  font-weight: bold;
+  color: #2c3e50;
+}
+
+nav a.router-link-exact-active {
+  color: #42b983;
+}
+</style>
---
+++ prueba-vue/src/views/HomeView.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="home">
+    <img class = "logoPacMan" alt="Vue logo" src="../assets/pac-man-logo.png">
+    <HelloWorld msg="Welcome to track system by VC"/>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import HelloWorld from '@/components/HelloWorld.vue'
+
+export default {
+  name: 'HomeView',
+  components: {
+    HelloWorld
+  }
+}
+</script>
+<style>
+.logoPacMan {
+  width: 400px;
+}
+</style>
